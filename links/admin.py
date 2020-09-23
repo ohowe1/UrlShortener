5b0ad25a26d7c4bff121afd88adb9d0df25fc7c9
o <ojhowe95@gmail.com>
2020-09-22 20:18:10 -0600

    Added the clicks and fixed the noninstant thing

--- links/admin.py
+++ links/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Link
+from .models import Link, Click
 # Register your models here.
-admin.site.register(Link)
+class ClickInline(admin.TabularInline):
+    model = Click
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    inlines = [
+        ClickInline
+    ]
+
--- links/views.py
+++ links/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Link
+from .models import Link, Click
+from django.conf import settings
+
 
 
 # Create your views here.
 def links(request, id):
+    context = {
+        "name": settings.NAME
+    }
     try:
         result = Link.objects.get(name=id)
     except Link.DoesNotExist:
-        return render(request, '404.html')
-    print(result)
-    result.clicks += 1
-    result.save()
+        return render(request, '404.html', context)
+    newclick = Click(link=result)
+    newclick.save()
+    print(result.click_set.all())
+    if not result.instant_redirect:
+        context["url"] = result.url
+        return render(request, 'noninstant.html', context)
     return HttpResponseRedirect(result.url)
