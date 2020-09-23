72c59ed65963181241aa804fd320159cac944fa8
o <ojhowe95@gmail.com>
2020-09-23 08:12:16 -0600

    Created dashboard app

--- shortify/settings.py
+++ shortify/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'links'
+    'links',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
