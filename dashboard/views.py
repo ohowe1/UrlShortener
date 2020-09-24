3c021075accde4ab7dbd97cb1755ea23e04fef45
o <ojhowe95@gmail.com>
2020-09-24 15:40:39 -0600

    Bootstrap and login alert along with login page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse, resolve
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+
+
+def generatecontext(request):
+    return {
+        'name': settings.NAME,
+        'logged': request.user.is_authenticated
+    }
+
+
 def index(request):
-    return render(request, 'dashindex.html')
+    context = generatecontext(request)
+    if not context['logged']:
+        return forwardtologin(request)
+    return render(request, 'dashindex.html', context)
+
+
+def login(request):
+    context = generatecontext(request)
+    if (request.session.get('loginalert')):
+        del request.session['loginalert']
+        context['alert'] = {
+            'type': 'danger',
+            'text': 'You need to be logged in to do that!'
+        }
+    return render(request, 'login.html', context)
+
+
+def forwardtologin(request):
+    request.session['loginalert'] = True
+    return HttpResponseRedirect(reverse('login', ))
